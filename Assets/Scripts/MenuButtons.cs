0a34482fbe397aaae0a51f01cae9e5a4aa42146d
h <d>
2021-09-25 22:34:56 -0400

    Main menu buttons are set up
    NEEDS TO HAVE CHANGE SCENE BUTTONS STILL

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayPress()
+    {
+
+    }
+    public void ExitPress()
+    {
+        UnityEngine.Application.Quit();
+    }
+}
